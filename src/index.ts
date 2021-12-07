4b36246146c1bc18071394f211f6c345b3786d53
skl <sun.keliang@foxmail.com>
2021-12-08 01:47:58 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 // https://juejin.cn/post/6901552013717438472#heading-15
+import { Command, program } from 'commander';
+import pkg from '../package.json';
 
-console.log(1212);
+// import pkg from './package.json';
+// console.log(process.env.npm_package_version);
+console.log(pkg.version);
+
+program.version('0.0.1');
