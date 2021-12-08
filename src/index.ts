e282e334da660b3f0e3bb8b1c5e4b26fc323bae4
wrynnsun <wrynnsun@tencent.com>
2021-12-08 16:10:36 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,14 @@ import pkg from '../package.json';
 // console.log(process.env.npm_package_version);
 console.log(pkg.version);
 
-program.version('0.0.1');
+program
+  .version('0.0.1')
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --show-log', '我最厉害');
+
+program.parse(process.argv);
+console.log(process.argv);
+
+const options = program.opts();
+
+console.log(options);
